bc1a119bf3e6d94b538eca87bde4663fa3cddcec
GGZ-hey <728611814@qq.com>
2020-02-09 15:43:32 +0800

    0

---
+++ 表白/表白/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace BBdemo
+{
+    /// <summary>
+    /// MainWindow. xaml 的交互逻辑
+    /// </suramary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Random rd = new Random();
+            Button btn = sender as Button;
+            double maxW = this.Width;
+            double maxH = this.Height;
+            double w = btn.Width;
+            double h = btn.Height;
+            double l = rd.Next(0, (int)(maxW - w));
+            double t = rd.Next(0, (int)(maxH - h));
+            btn.Margin = new Thickness(l, t, 0, 0);
+        }
+        private void closing_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+           
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            labl.Visibility = System.Windows.Visibility.Hidden;
+            lab2.Content = "谢谢媳妇！";
+            btn3.Visibility = System.Windows.Visibility.Visible;
+            btnl.Visibility = System.Windows.Visibility.Hidden;
+            btn2.Visibility = System.Windows.Visibility.Hidden;
+        }
+        private void Button_Click_l(object sender, RoutedEventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBox.Show(" 关不掉！");
+            e.Cancel = true;
+        }
+    }
+}
